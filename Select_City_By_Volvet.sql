abfd63482455b00d3038b098c638f9542232beeb
Alexey <nafiguspofigus@gmail.com>
2018-09-01 00:36:51 +0300

    Update Select_City_By_Volvet.sql

--- Select_City_By_Volvet.sql
+++ Select_City_By_Volvet.sql
@@ -33,3 +33,11 @@ WHERE CITY LIKE '%[^AEIOU]';
 --Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY LIKE '%[^AEIUO]' AND CITY LIKE '[^AEIOU]%';
+
+--Query the Name of any student in STUDENTS who scored higher than  Marks. 
+--Order your output by the last three characters of each name. 
+---If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), 
+--secondary sort them by ascending ID.
+SELECT NAME FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID ASC;
