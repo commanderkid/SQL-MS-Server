0d4df4ad849f723ebf016f0f51805e08b26c87bc
Alexey <nafiguspofigus@gmail.com>
2018-09-01 00:52:59 +0300

    Update Select_City_By_Volvet.sql

--- Select_City_By_Volvet.sql
+++ Select_City_By_Volvet.sql
@@ -41,3 +41,11 @@ WHERE CITY LIKE '%[^AEIUO]' AND CITY LIKE '[^AEIOU]%';
 SELECT NAME FROM STUDENTS
 WHERE Marks > 75
 ORDER BY RIGHT(Name, 3), ID ASC;
+
+
+--Write a query that prints a list of employee names (i.e.: the name attribute) 
+--for employees in Employee having a salary greater than  per month who have been employees for less than  months. 
+--Sort your result by ascending employee_id.
+SELECT NAME FROM Employee
+WHERE months < 10 AND salary > 2000
+ORDER BY employee_id ASC;
